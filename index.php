6dbe826a21ddb0b2de4d9e5e99830c5b370dbf43
marescanog <marescanog@gmail.com>
2021-10-25 10:45:59 +0800

    adding images and changed inline styles to non inline

--- index.php
+++ index.php
@@ -17,10 +17,10 @@ require_once dirname(__FILE__).'/components/head-meta.php';
     <!-- === Your Custom Page Content Goes Here below here === -->
 
     <div class="jumbotron py-0 mb-auto d-flex flex-end">
-        <div class="d-lg-flex flex-lg-row-reverse justify-content-center" style="width:100%;margin: auto; height:100%;">
+        <div class="d-lg-flex flex-lg-row-reverse justify-content-center jumbotron-container">
             <div class="d-flex">
                 <div class="mx-auto m-lg-0 d-flex flex-end justify-content-center">
-                    <img src="./images/pages/landing/Jumbotron_Image.jpg" alt="" class="img-fluid" style="max-height: 90%;  width: auto; margin-top:auto">
+                    <img src="./images/pages/landing/Jumbotron_Image.jpg" alt="" class="img-fluid jumbotron-img" >
                 </div>
             </div>
 
@@ -47,11 +47,79 @@ require_once dirname(__FILE__).'/components/head-meta.php';
 
     </div>
 
-    <div style="min-height:540px; background-color:#E4E4E4; z-index:200">
+    <div class="pop-post_container">
+        <div class="container pop-post_content-wrapper">
+            <h4 class="txt-semi">Popular Postings in your area</h4>
+
+            <div id="carouselExampleControls" class="carousel slide"  data-interval="false" data-ride="carousel" data-pause="hover">
+            <div class="carousel-inner">
+                <div class="carousel-item d-flex active">
+                    <div class="card">
+                        <div class="card-body">
+                            This is some text within a card body.
+                        </div>
+                    </div>
+                    <div class="card">
+                        <div class="card-body">
+                            This is some text within a card body.
+                        </div>
+                    </div>
+                    <div class="card">
+                        <div class="card-body">
+                            This is some text within a card body.
+                        </div>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <div style="background-color:#0000FF; width: 100%; height: 100%">
+                            d
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <div style="background-color:#0000FF; width: 100%; height: 100%">
+                            d
+                    </div>
+                </div>
+            </div>
+            <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </a>
+            </div>
+        </div>
+
+    </div>
+
+    <div style="min-height:540px; background-color:#FFFFFF; z-index:200">
         <div class="container pt-5">
-             <h6>Popular Postings in your area</h6>
+             <h6>How it works</h6>
         </div>
     </div>
+
+    <div style="min-height:540px; background-color:#E4E4E4; z-index:200">
+        <div class="container pt-5">
+             <h6>Testimony</h6>
+        </div>
+    </div>
+
+    <div style="min-height:540px; background-color:#FFFFFF; z-index:200">
+        <div class="container pt-5">
+             <h6>Featured Heroes</h6>
+        </div>
+    </div>
+
+    <div style="min-height:540px; background-color:#E4E4E4; z-index:200">
+        <div class="container pt-5">
+             <h6>ready to get your needs solved?</h6>
+        </div>
+    </div>
+
+
+    
 
 
     <!-- === Your Custom Page Content Goes Here above here === -->
