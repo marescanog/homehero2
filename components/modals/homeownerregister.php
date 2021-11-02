a463ebef7e818d0f582311a706f954d07847e298
20100215 <20100215@usc.edu.ph>
2021-11-02 14:29:03 +0800

    edited content to match given template

---
+++ components/modals/homeownerregister.php
@@ -0,0 +1,39 @@
+<div class="modal-content">
+    <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+        </button>
+    </div>
+    <div class="modal-body">
+        <form>
+            <div class="d-flex">
+                <div class="form-group w-100 ml-2">
+                    <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="FIrst name">
+                </div>
+                <div class="form-group w-100 ml-2">
+                    <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Last name">
+                </div>
+            </div>
+            <div class="form-group">
+                <input type="text" class="form-control" id="email" placeholder="Email address">
+            </div>
+            <div class="form-group">
+                <label for="exampleInputPassword1">Password</label>
+                <input type="password" class="form-control" id="exampleInputPassword1">
+            </div>
+            <div class="form-group">
+                <label for="exampleInputPassword1">Confirm Password</label>
+                <input type="password" class="form-control" id="exampleInputPassword1">
+            </div>
+            <div class="form-group form-check">
+                <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                <label class="form-check-label" for="exampleCheck1">I agree to the <a href="">terms and conditions</a></label>
+            </div>
+            <button type="button" class="btn btn-warning text-white w-100 mt-3" data-toggle="modal" data-target="#exampleModal">Submit</button>
+        </form>
+    </div>
+    <div class="modal-footer">
+        
+    </div>
+</div>
