8c675a054e4312b09611ada47b278d30ae0cab49
20100215 <20100215@usc.edu.ph>
2021-11-02 14:26:42 +0800

    edited modal to align with given template

--- components/modals/smsverification.php
+++ components/modals/smsverification.php
@@ -1,32 +1,25 @@
-
-
-        <!-- Modal -->
-        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-            <div class="modal-dialog">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                            <span aria-hidden="true">&times;</span>
-                        </button>
-                    </div>
-                    <div class="modal-body">
-                        <h4>Verify SMS</h4>
-                        <p>Enter verification code sent to <?php echo "0912 345 6789"?></p>
-                        <form>
-                            <div class="d-flex">
-                                <input class="form-control mr-1" type="text" style="width:30px; height:43px; font-size:2rem; border:0px 0px 2px 0px solid black;" placeholder="0">
-                                <input class="form-control mr-1" type="text" style="width:30px; height:43px; font-size:2rem; border:0px 0px 2px 0px solid black;" placeholder="0">
-                                <input class="form-control mr-1" type="text" style="width:30px; height:43px; font-size:2rem; border:0px 0px 2px 0px solid black;" placeholder="0">
-                                <input class="form-control mr-1" type="text" style="width:30px; height:43px; font-size:2rem; border:0px 0px 2px 0px solid black;" placeholder="0">
-                            </div>
-                            <button class="btn btn-warning text-white">Verify</button>
-                        </form>
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                        <button type="button" class="btn btn-primary">Save changes</button>
-                    </div>
-                </div>
+<div class="modal-content">
+    <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+        </button>
+    </div>
+    <div class="modal-body">
+        <h4>Verify SMS</h4>
+        <p>Enter verification code sent to <?php echo "0912 345 6789" ?></p>
+        <form>
+            <div class="d-flex">
+                <input class="form-control mr-1" type="text" style="width:30px; height:43px; font-size:2rem; border:0px 0px 2px 0px solid black;" placeholder="0">
+                <input class="form-control mr-1" type="text" style="width:30px; height:43px; font-size:2rem; border:0px 0px 2px 0px solid black;" placeholder="0">
+                <input class="form-control mr-1" type="text" style="width:30px; height:43px; font-size:2rem; border:0px 0px 2px 0px solid black;" placeholder="0">
+                <input class="form-control mr-1" type="text" style="width:30px; height:43px; font-size:2rem; border:0px 0px 2px 0px solid black;" placeholder="0">
             </div>
-        </div>
+            <button class="btn btn-warning text-white">Verify</button>
+        </form>
+    </div>
+    <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+    </div>
+</div>
