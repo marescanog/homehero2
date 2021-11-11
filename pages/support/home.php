fae26e38c70a60d1d5346107a6b096eb3a26144e
marescanog <marescanog@gmail.com>
2021-11-11 21:11:15 +0800

    finished the dashboard page for support

--- pages/support/home.php
+++ pages/support/home.php
@@ -28,11 +28,84 @@ require_once dirname(__FILE__)."/$level/components/head-meta.php";
     <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pb-2 mb-3 border-bottom">
         <h1 class="h2">Dashboard</h1>
     </div>
+        <!-- Cards -->
+        <div class="row mb-4">
+            <div class="col-12 col-lg-3">
+                <div class="card">
+                <div class="card-body purple">
+                    <div class="d-flex align-items-center justify-content-between">
+                        <div class="p-0 cont-value">
+                            <?php
+                                echo isset($_POST['new_ticket_total']) ? $_POST['new_ticket_total'] : '0';
+                            ?>
+                        </div>
+                        <div class="p-0 cont-icon">
+                            <i class="fas fa-ticket-alt card-icon purple-icon"></i>
+                        </div>
+                    </div>
+                    <h5 class="card-title card-header-format">New Tickets</h5>
+                </div>
+                </div>
+            </div>
+
+            <div class="col-12 col-lg-3">
+                <div class="card ">
+                <div class="card-body blue">
+                    <div class="d-flex align-items-center justify-content-between">
+                        <div class="p-0 cont-value">
+                            <?php
+                                echo isset($_POST['ongoing_ticket_total']) ? $_POST['ongoing_ticket_total'] : '0';
+                            ?>
+                        </div>
+                        <div class="p-0 cont-icon">
+                            <i class="fas fa-list-alt card-icon blue-icon"></i>
+                        </div>
+                    </div>
+                    <h5 class="card-title card-header-format">Ongoing Tickets</h5>
+                </div>
+                </div>
+            </div>
+
+            <div class="col-12 col-lg-3">
+                <div class="card">
+                <div class="card-body green">
+                    <div class="d-flex align-items-center justify-content-between">
+                        <div class="p-0 cont-value">
+                            <?php
+                                echo isset($_POST['resolved_ticket_total']) ? $_POST['resolved_ticket_total'] : '0';
+                            ?>
+                        </div>
+                        <div class="p-0 cont-icon">
+                            <i class="fas fa-calendar-check card-icon green-icon"></i>
+                        </div>
+                    </div>
+                    <h5 class="card-title card-header-format">Resolved Tickets</h5>
+                </div>
+                </div>
+            </div>
+        </div>
 
     <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pb-2 mb-3 border-bottom">
         <h1 class="h2">Anouncements</h1> 
     </div>
-</main>
+    <div>
+        <?php 
+            if(isset($_POST['anouncements'])) {
+                for($x = 0; $x < count($_POST['anouncements']); $x++){
+        ?>
+        <ul>
+            <?php 
+                echo "<li>".$_POST['anouncements'][$x]."</li>";
+            ?>
+        </ul>
+        <?php
+                }
+            } else {
+                echo 'No Anouncements';
+            }
+        ?>
+    </div>
+    </main>
 
 
 
