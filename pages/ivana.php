343c540320c40d682abfe77694bbf0a8788e0ccb
marescanog <marescanog@gmail.com>
2021-11-11 13:06:23 +0800

    restoring changes

---
+++ pages/ivana.php
@@ -0,0 +1,38 @@
+<?php 
+
+$level ="..";
+require_once dirname(__FILE__)."/$level/components/head-meta.php"; 
+
+?>
+<!-- === Link your custom CSS pages below here ===-->
+<link rel="stylesheet" href="../css/headers/user.css">
+
+<!-- === Link your custom CSS  pages above here ===-->
+</head>
+ <body class="container-fluid m-0 p-0 main-container">  
+    <!-- Add your Header NavBar here-->
+    <?php 
+        require_once dirname(__FILE__)."/$level/components/headers/user.php"; 
+    ?>
+    <div class="<?php echo $hasHeader ?? ""; ?>">
+    <!-- === Your Custom Page Content Goes Here below here === -->
+
+
+    <h1>Ivana</h1>
+
+    
+
+
+
+
+
+
+    <!-- === Your Custom Page Content Goes Here above here === -->
+    </div>
+<?php require_once dirname(__FILE__).'/../components/foot-meta.php'; ?>
+<!-- Custom JS Scripts Below -->
+    <script>
+
+    </script>
+</body>
+</html>
