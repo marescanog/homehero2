a2fb0df5c8196d0c2ece8e1db79b6dbd99aaf014
marescanog <marescanog@gmail.com>
2021-11-11 23:22:05 +0800

    completed the logic for the table element

--- components/UX/support-table.php
+++ components/UX/support-table.php
@@ -1,5 +1,155 @@
+<?php
+// Variables for the DOM Elements
+    $basicSearchId = isset($basicSearchId) ? $basicSearchId : "";
+
+// Variables for the "Showing X out of x"
+    $totalRecords = isset($totalRecords) ? $totalRecords : 0;
+    $EntriesDisplayed = isset($EntriesDisplayed) ? $EntriesDisplayed : 0;
+
+// Variables for the tables
+    $tableName = isset($tableName) ? $tableName: null;
+
+    $tableHeaderLabels = isset($tableHeaderLabels) ? $tableHeaderLabels : 
+    ["Ticket No.", "Type", "Status", "Assigned Agent", "Last Updated", "Date Assigned", "Date Created"];
+
+    $tableRows = [
+        ["REG-001", "Registration", "Ongoing", "Ashley Miles", "09/12/2021", "09/12/2021", "09/12/2021", "1"],
+        ["DIS-002", "Dispute", "New", "Jim Day", "09/12/2021", "09/12/2021", "09/12/2021", "2"],
+        ["REG-003", "Registration", "Resolved", "Ashley Miles", "09/12/2021", "09/12/2021", "09/12/2021", "3"],
+    ];
+
+    $tableRows = isset($tableRows) ? $tableRows : [];
+
+    $statusButton = isset($statusButton) ? $statusButton : 3;
+
+// Variables for the pagination
 
 
-<div>
-    
+?>
+
+<div class="main-container d-flex flex-column flex-1 min-height">
+    <div>
+        <div class="mt-3 row d-flex justify-content-between align-items-center">
+            <div class="col-12 col-lg-2 ">
+                <p class="showing-x-text ml-2">
+                    Showing <?php echo $EntriesDisplayed ;?> 
+                    out of <?php echo $totalRecords;?> 
+                    Entries  
+                </p>
+            </div>
+            <div class="input-group mb-3 col-12 col-lg-3 mr-2">
+                <div class="input-group-prepend  ">
+                    <span class="input-group-text override-input-group" 
+                    <?php 
+                        echo $basicSearchId == "" ? "" : "id='$basicSearchId'";
+                    ?>
+                    >
+                        <i class="fas fa-search"></i>
+                    </span>
+                </div>
+                <input type="text" class="form-control override-input" placeholder="Search Ticket" aria-label="search" aria-describedby="basic-search">
+            </div>
+        </div>
+        <div class="table-responsive">
+            <table class="table table-striped table-sm">
+                <!-- TABLE HEADER DATA -->
+                <thead>
+                <tr>
+                    <?php 
+                        for($x = 0; $x < count($tableHeaderLabels); $x++){
+                    ?>
+                    <th><?php echo $tableHeaderLabels[$x] ;?></th>
+                    <?php
+                        }
+                    ?>
+                </tr>
+                </thead>
+
+                <!-- TABLE BODY DATA -->
+                <?php 
+                    if(count($tableRows) > 0){
+                ?>
+                <tbody>
+                    <?php
+                        for($x=0; $x<count($tableRows); $x++){
+                    ?>
+                    <tr>
+                        <?php 
+                            for($y=0; $y < count($tableRows[$x]) -1; $y++){
+                                if(isset($statusButton) && $y == $statusButton-1){
+                        ?>
+                            <td class="pr-4">
+                                <button 
+                                    <?php 
+                                        echo  $tableName == null ? "" :
+                                        "id='".$tableName."-".$tableRows[$x][count($tableRows[0])-1]."'";
+                                    ?>
+                                class="btn btn-primary btn-table
+                                    <?php echo $tableRows[$x][$y];?>
+                                ">
+                                    <?php echo $tableRows[$x][$y];?>
+                                </button>
+                            </td>
+                        <?php
+                                } else {
+                        ?>
+                                    <td><?php echo $tableRows[$x][$y];?></td>
+                        <?php 
+                                }
+                            }
+                        ?>
+                    </tr>
+                    <?php 
+                        }
+                    ?>
+                </tbody>
+                <?php 
+                    } 
+                ?>
+            </table>
+        </div>
+        <?php 
+            if(count($tableRows) == 0){
+        ?>
+            <div>
+                <p>No Results Found.</p>
+            </div>
+        <?php 
+            } 
+        ?>
+    </div>
+    <!-- Footer -->
+    <div class="mt-auto d-flex flex-column flex-lg-row justify-content-between align-items-center">
+        <!-- Number of Entries -->
+        <div class="btn-group show-entries-height">
+            <button class="btn btn-secondary btn-sm dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Show 10 Entries
+            </button>
+            <div class="dropdown-menu">
+                <button class="dropdown-item" type="button">Show 10 Entries</button>
+                <button class="dropdown-item" type="button">Show 20 Entries</button>
+                <button class="dropdown-item" type="button">Show 30 Entries</button>
+            </div>
+        </div>
+        <!-- Pagination -->
+        <nav aria-label="Page navigation example">
+            <ul class="pagination">
+                <li class="page-item">
+                <a class="page-link" href="#" aria-label="Previous">
+                    <span aria-hidden="true">&laquo;</span>
+                    <span class="sr-only">Previous</span>
+                </a>
+                </li>
+                <li class="page-item"><a class="page-link" href="#">1</a></li>
+                <li class="page-item"><a class="page-link" href="#">2</a></li>
+                <li class="page-item"><a class="page-link" href="#">3</a></li>
+                <li class="page-item">
+                <a class="page-link" href="#" aria-label="Next">
+                    <span aria-hidden="true">&raquo;</span>
+                    <span class="sr-only">Next</span>
+                </a>
+                </li>
+            </ul>
+        </nav>
+    </div>
 </div>
