e2374072193e1bbc4381291ecc21713e98e8efbd
marescanog <marescanog@gmail.com>
2021-11-15 12:57:20 +0800

    adding ipad pro spacing adjustments

--- pages/homeowner/create-project.php
+++ pages/homeowner/create-project.php
@@ -28,57 +28,53 @@ require_once dirname(__FILE__)."/$level/components/head-meta.php";
     <div class="min-body-height d-flex flex-column justify-content-between <?php echo $hasHeader ?? ""; ?>">
     <!-- === Your Custom Page Content Goes Here below here === -->
 
-<div class="container container-full  w-100 m-0 p-0 min-height h-100">
-    <div class="min-height content-container row flex-lg-row flex-column-reverse  m-auto ">
-        <!-- Info Block -->
-        <div class="info-container col-12 col-lg-3 min-height">
-            <div class="info-wrapper flex-1">
-                <h3>Create a project for Plumbing Repair</h3>
+    <!-- IPad Pro spacing adjustment -->
+    <div class="container container-full d-flex w-100 m-0 p-0 h-100 ipad">
+        <div class="content-container  d-flex row flex-lg-row flex-column-reverse mx-auto ipad">
+            <div class="info-container col-12 col-lg-3"></div>
+            <div class="col-12 col-lg-9"></div>
+        </div>
+    </div>
 
-                <div class="img-container">
-                    <img src="../../images/illustrations/plumbing.jpg" class="img-fluid" alt="Responsive image">
+    <!-- Main Content -->
+    <div class="container container-full  w-100 m-0 p-0 min-height h-100">
+        <div class="min-height content-container row flex-lg-row flex-column-reverse  m-auto ">
+            <!-- Info Block -->
+            <div class="info-container col-12 col-lg-3 min-height">
+                <div class="info-wrapper flex-1">
+                    <h3>Create a project for Plumbing Repair</h3>
+
+                    <div class="img-container">
+                        <img src="../../images/illustrations/plumbing.jpg" class="img-fluid" alt="Responsive image">
+                    </div>
+                    
+                    <p>Specify your project details to match up with the right HomeHero</p>
+
+                    <h4>We match HomeHeroes by:</h4>
+                    <ul>
+                        <li>Location</li>
+                        <li>Services offered</li>
+                        <li>Schedule</li>
+                    </ul>
+
+                    <h4>Looking for a different service?</h4>
+                    <a href="">Select another project category</a>
                 </div>
-                
-                <p>Specify your project details to match up with the right HomeHero</p>
-
-                <h4>We match HomeHeroes by:</h4>
-                <ul>
-                    <li>Location</li>
-                    <li>Services offered</li>
-                    <li>Schedule</li>
-                </ul>
-
-                <h4>Looking for a different service?</h4>
-                <a href="">Select another project category</a>
+            </div>
+            <!-- Form Block -->
+            <div class="min-height col-12 col-lg-9 form">
+                <h2>Choose the best time for you</h2>
             </div>
         </div>
-        <!-- Form Block -->
-        <div class="min-height col-12 col-lg-9 form">
-            <h2>Choose the best time for you</h2>
-        </div>
-        
     </div>
-</div>
-    
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
-
-
+    <!-- IPad Pro spacing adjustment -->
+    <div class="container container-full d-flex w-100 m-0 p-0 h-100 ipad">
+        <div class="content-container  d-flex row flex-lg-row flex-column-reverse mx-auto ipad">
+            <div class="info-container col-12 col-lg-3"></div>
+            <div class="col-12 col-lg-9"></div>
+        </div>
+    </div>
 
 
 <?php 
