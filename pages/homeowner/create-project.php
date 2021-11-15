261dcc9bbee425d92e613b70582002eb14cc544c
marescanog <marescanog@gmail.com>
2021-11-15 10:05:38 +0800

    fiexed horizontal scrolling issue on mobile

--- pages/homeowner/create-project.php
+++ pages/homeowner/create-project.php
@@ -20,7 +20,7 @@ require_once dirname(__FILE__)."/$level/components/head-meta.php";
 <link rel="stylesheet" href="../../css/pages/homeowner/homeowner-create-project.css">
 <!-- === Link your custom CSS  pages above here ===-->
 </head>
- <body class="container-fluid m-0 p-0 main-container">  
+ <body class="container-fluid m-0 p-0  w-100">  
     <!-- Add your Header NavBar here-->
     <?php 
         require_once dirname(__FILE__)."/$level/components/headers/ho-signed-in.php"; 
@@ -28,26 +28,35 @@ require_once dirname(__FILE__)."/$level/components/head-meta.php";
     <div class="<?php echo $hasHeader ?? ""; ?>">
     <!-- === Your Custom Page Content Goes Here below here === -->
 
-<div class="container-fluid m-0 p-0 min-height">
-    <div class="min-height content-container row flex-lg-row flex-column-reverse">
+<div class="container  w-100 m-0 p-0 min-height">
+    <div class="min-height content-container row flex-lg-row flex-column-reverse ">
+        <!-- Info Block -->
         <div class="info col-12 col-lg-3  min-height">
-            <h3>Create a project for Plumbing Repair</h3>
-            <p>Picture</p>
-            <p>Specify your project details to match up with the right HomeHero</p>
+            <div>
+                <h3>Create a project for Plumbing Repair</h3>
 
-            <h4>We match HomeHeroes by:</h4>
-            <ul>
-                <li>Location</li>
-                <li>Services offered</li>
-                <li>Schedule</li>
-            </ul>
+                <div class="img-container">
+                    <img src="../../images/illustrations/plumbing.jpg" class="img-fluid" alt="Responsive image">
+                </div>
+                
+                <p>Specify your project details to match up with the right HomeHero</p>
 
-            <h4>Looking for a different service?</h4>
-            <a href="">Select another project category</a>
+                <h4>We match HomeHeroes by:</h4>
+                <ul>
+                    <li>Location</li>
+                    <li>Services offered</li>
+                    <li>Schedule</li>
+                </ul>
+
+                <h4>Looking for a different service?</h4>
+                <a href="">Select another project category</a>
+            </div>
         </div>
+        <!-- Form Block -->
         <div class="min-height col-12 col-lg-9 form">
             <h2>Choose the best time for you</h2>
         </div>
+        
     </div>
 </div>
     
