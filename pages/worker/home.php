7bd3948a27a930635c0ad0d5ec8bf69078b28085
marescanog <marescanog@gmail.com>
2021-11-16 20:18:00 +0800

    added base pages for home and landing

--- pages/worker/home.php
+++ pages/worker/home.php
@@ -1,25 +1,44 @@
 <?php 
 
-$level ="../../";
+// session_start();
+// if(!isset($_SESSION["token"])){
+//     header("Location: ../../");
+//     exit();
+// }
+
+$level ="../..";
+$fistName = isset($_SESSION["first_name"]) ? $_SESSION["first_name"] : "Guest";
+$initials = isset($_SESSION["initials"]) ? $_SESSION["initials"] : "GU";
+
+
 require_once dirname(__FILE__)."/$level/components/head-meta.php"; 
 
 ?>
 <!-- === Link your custom CSS pages below here ===-->
-
-
+<link rel="stylesheet" href="../../css/headers/header-homeowner.css">
+<link rel="stylesheet" href="../../css/footer.css">
+<link rel="stylesheet" href="../../css/pages/homeowner/homeowner-create-project.css">
 <!-- === Link your custom CSS  pages above here ===-->
 </head>
- <body class="container-fluid m-0 p-0 main-container">  
+ <body class="container-fluid m-0 p-0  w-100">  
     <!-- Add your Header NavBar here-->
     <?php 
-        require_once dirname(__FILE__)."/$level/components/headers/user.php"; 
+        require_once dirname(__FILE__)."/$level/components/headers/ho-signed-in.php"; 
     ?>
     <div class="<?php echo $hasHeader ?? ""; ?>">
     <!-- === Your Custom Page Content Goes Here below here === -->
 
+<div class="container container-full  w-100 m-lg-0 p-0 min-height">
+    
+<h1>Worker Home Page</h1>
 
-    <h1>Worker Home</h1>
 
+
+
+
+
+
+</div>
     
 
 
@@ -27,11 +46,33 @@ require_once dirname(__FILE__)."/$level/components/head-meta.php";
 
 
 
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+<?php 
+    require_once dirname(__FILE__)."/$level/components/footer.php"; 
+?>
+
     <!-- === Your Custom Page Content Goes Here above here === -->
     </div>
 <?php require_once dirname(__FILE__)."/$level/components/foot-meta.php"; ?>
 <!-- Custom JS Scripts Below -->
+    <!-- <script src="../../js/pages/user-home.js"></script> -->
     <script>
+
+   
 
     </script>
 </body>
--- pages/worker/landing.php
+++ pages/worker/landing.php
@@ -1,15 +1,24 @@
 <?php 
 
-$level ="../../";
+// session_start();
+// if(!isset($_SESSION["token"])){
+//     header("Location: ../../");
+//     exit();
+// }
+
+$level ="../..";
+
+
 require_once dirname(__FILE__)."/$level/components/head-meta.php"; 
 
 ?>
 <!-- === Link your custom CSS pages below here ===-->
-
-
+<link rel="stylesheet" href="../../css/headers/user.css">
+<link rel="stylesheet" href="../../css/footer.css">
+<link rel="stylesheet" href="../../css/landing.css">
 <!-- === Link your custom CSS  pages above here ===-->
 </head>
- <body class="container-fluid m-0 p-0 main-container">  
+ <body class="container-fluid m-0 p-0  w-100">  
     <!-- Add your Header NavBar here-->
     <?php 
         require_once dirname(__FILE__)."/$level/components/headers/user.php"; 
@@ -17,9 +26,26 @@ require_once dirname(__FILE__)."/$level/components/head-meta.php";
     <div class="<?php echo $hasHeader ?? ""; ?>">
     <!-- === Your Custom Page Content Goes Here below here === -->
 
+<div class="container container-full  w-100 m-lg-0 p-0 min-height">
 
-    <h1>Worker landing</h1>
 
+<h1>Worker Landing Page</h1>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</div>
     
 
 
@@ -27,11 +53,33 @@ require_once dirname(__FILE__)."/$level/components/head-meta.php";
 
 
 
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+<?php 
+    require_once dirname(__FILE__)."/$level/components/footer.php"; 
+?>
+
     <!-- === Your Custom Page Content Goes Here above here === -->
     </div>
 <?php require_once dirname(__FILE__)."/$level/components/foot-meta.php"; ?>
 <!-- Custom JS Scripts Below -->
+    <!-- <script src="../../js/pages/user-home.js"></script> -->
     <script>
+
+   
 
     </script>
 </body>
