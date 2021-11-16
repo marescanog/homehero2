070aebca0bc5fc5b5a13feb9eed4b8ef15f5b3e0
marescanog <marescanog@gmail.com>
2021-11-16 16:08:05 +0800

    added page 2 base

--- components/forms/UCPFC-2.php
+++ components/forms/UCPFC-2.php
@@ -8,17 +8,53 @@
     // var_dump($_POST);
 ?>
 
-<h1>Second page</h1>
-
+<h5 class="text-left">Project Name:</h5>
 <div class="form-group">
-    <label class="text-left" for="text2">Text</label>
-    <input type="text" class="form-control" id="text2" name="text2" placeholder="text" 
-    value=<?php 
-        if($bci_page_2 != null){
-            echo isset($bci_page_2["text2"]) ? $bci_page_2["text2"] : "";
-        }
-    ?>>
+    <input type="text" class="form-control" id="project_name" name="project_name" placeholder="Plumbing Repair">
+</div>
 
+<h5 class="text-left">Description:</h5>
+<div class="form-group">
+<textarea class="form-control" id="project_description" name="project_description" rows="3"></textarea>
+</div>
+
+<h6>How big is your project?</h6>
+<div class="d-flex flex-row">
+    <div class="form-check">
+        <input class="form-check-input" type="radio" name="jobSize" id="jobSize_small" value="small" checked>
+        <label class="form-check-label" for="jobSize_small">
+            Small - Est. 1 - 4 hrs.
+        </label>
+    </div>
+    <div class="form-check">
+        <input class="form-check-input" type="radio" name="jobSize" id="jobSize_Medium" value="medium">
+        <label class="form-check-label" for="jobSize_Medium">
+            Medium - Est 4 - 8 hrs.
+        </label>
+    </div>
+    <div class="form-check">
+        <input class="form-check-input" type="radio" name="jobSize" id="jobSize_large" value="large">
+        <label class="form-check-label" for="jobSize_large">
+            Large - Est 8+ hrs.
+        </label>
+    </div>
+</div>
+
+<h6>Set an estimated offer</h6>
+<div>
+    <div class="form-group">
+        <label for="rateValue">P</label>
+        <input type="number" class="form-control" id="rateValue" placeholder="Password" name="rateValue">
+    </div>
+    <select class="custom-select">
+        <option selected value="1">per hour</option>
+        <option value="2">per day</option>
+        <option value="3">per week</option>
+        <option value="3">per project</option>
+    </select>
+</div>
+
+<div>
     <button type="button" id="btn-back-page-2" class="btn btn-outline-warning">
         Back
     </button>
@@ -26,6 +62,17 @@
     <button type="button" id="btn-page-2" class="btn btn-warning text-white">
         Next
     </button>
+</div>
+
+
+<div class="form-group">
+    <!-- <label class="text-left" for="text2">Text</label>
+    <input type="text" class="form-control" id="text2" name="text2" placeholder="text" 
+    value=<?php 
+        // if($bci_page_2 != null){
+        //     echo isset($bci_page_2["text2"]) ? $bci_page_2["text2"] : "";
+        // }
+    ?>> -->
 
     <!-- Hidden Feilds -->
     <input type="hidden" class="form-control" id="text1" name="text1" placeholder="text" 
