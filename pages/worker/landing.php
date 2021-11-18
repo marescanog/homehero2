cefaeade27695f8f838e560fa259be34586466f5
marescanog <marescanog@gmail.com>
2021-11-19 06:04:28 +0800

    worker landing page base set-up

--- pages/worker/landing.php
+++ pages/worker/landing.php
@@ -1,86 +1,168 @@
 <?php 
 
 // session_start();
-// if(!isset($_SESSION["token"])){
-//     header("Location: ../../");
+// if(isset($_SESSION["token"])){
+//     header("Location: ./pages/homeowner/home.php");
 //     exit();
 // }
 
 $level ="../..";
-
-
 require_once dirname(__FILE__)."/$level/components/head-meta.php"; 
-
 ?>
-<!-- === Link your custom CSS pages below here ===-->
+<!-- Add your custom CSS below -->
 <link rel="stylesheet" href="../../css/headers/user.css">
-<link rel="stylesheet" href="../../css/footer.css">
 <link rel="stylesheet" href="../../css/landing.css">
-<!-- === Link your custom CSS  pages above here ===-->
+<link rel="stylesheet" href="../../css/footer.css">
+<!-- Add your custom CSS above -->
 </head>
- <body class="container-fluid m-0 p-0  w-100">  
+ <body class="container-fluid m-0 p-0 main-container">  
     <!-- Add your Header NavBar here-->
     <?php 
         require_once dirname(__FILE__)."/$level/components/headers/user.php"; 
     ?>
-    <div class="<?php echo $hasHeader ?? ""; ?>">
+    <div class="<?php echo $hasHeader ?? ''; ?>">
     <!-- === Your Custom Page Content Goes Here below here === -->
 
-<div class="container container-full  w-100 m-lg-0 p-0 min-height">
+    <!-- =============================================== -->
+    <!-- JUMBOTRON -->
+    <!-- =============================================== -->
+    <div class="jumbotron py-0 mb-auto d-flex justify-content-center flex-lg-end">
+        <div class="d-lg-flex flex-lg-row justify-content-center jumbotron-container row">
+            <div class="d-flex jumbotron-img-container col-12 col-lg-6 mt-auto">
+                <div class="mx-auto m-lg-0 d-flex justify-content-center">
+                    <!-- <img src="./images/pages/landing/Jumbotron_Image.jpg" 
+                         srcset="./images/pages/landing/Jumbotron_Image.jpg 594w, 
+                                 ./images/pages/landing/jumbo-320.jpg 320w,
+                                 ./images/pages/landing/jumbo-280.jpg 280w"
+                         sizes="(max-width: 280px) 280w, 
+                                (max-width: 320px) 320w
+                                594w"
+                         alt="A homehero worker holding a drill" 
+                         class="img-fluid jumbotron-img" > -->
+                </div>
+            </div>
 
-
-<h1>Worker Landing Page</h1>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-</div>
-    
-
-
-
-
-
-
-
-
-
-
-
+            <div class="jumbotron-card mt-5 pt-5 col-12 col-lg-6" > 
+                <div class="card w-lg-75 ml-lg-auto mr-lg-3 mb-lg-5 jumbotron-card-body">
+                    <div class="card-body">
+                        
+             
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 
 
 
+    <!-- =============================================== -->
+    <!-- HOW IT WORKS -->
+    <!-- =============================================== -->
+    <div class="feature-point-container">
+        <div class="container featured-point_content-wrapper">
+            <div class="container pt-5">
+                <h2 class="gray-font feature-point-headline text-center">How it works</h2>
+
+                <div class="row d-flex justify-content-around mt-5 mb-5 mb-lg-0"> 
 
 
-    
+                    <!-- Feature 1 -->
+                    <div class="col col-lg-3 d-flex flex-column align-items-center pb-5">
+                        <div>
+                            <?php
+                                // include dirname(__FILE__).'/images/pages/landing/post_tasks.svg'; 
+                            ?>
+                        </div>
+                        <div>
+                            <h4 class="gray-font text-center">Post Tasks</h4>
+                            <p>Delegate projects that need to be done by posting your needs</p>
+                        </div>
+                    </div> 
+
+                    
+                    <!-- Feature 2 -->
+                     <div class="col col-lg-3 d-flex flex-column align-items-center">
+                        <div>
+                            <?php
+                                // include dirname(__FILE__).'/images/pages/landing/review_offers.svg'; 
+                            ?>
+                        </div>
+                        <div>
+                            <h4 class="gray-font text-center">Review Offers</h4>
+                            <p>Check offers and hire the right hero for your tasks</p>
+                        </div>
+                    </div> 
+
+                    
+                    <!-- Feature 1 -->
+                    <div class="col col-lg-3 d-flex flex-column align-items-center">
+                        <div >
+                            <!-- <img src="./images/pages/landing/taxi-surfing.png" alt="" class="img-fluid feature-max-img"> -->
+                        </div>
+                        <div>
+                            <h4 class="gray-font text-center">Task Done!</h4>
+                            <p>Sit back and relax as your hirehero gets the task done for you!</p>
+                        </div>
+                    </div> 
 
 
+                </div>
+            </div>
+        </div>
+    </div>
 
+  
+ 
+    <!-- =============================================== -->
+    <!-- CTA BOTTOM  -->
+    <!-- =============================================== -->
+    <div class="CTA-container pb-5 pb-lg-0">
+        <div class="container CTA-wrapper pt-5 ">
+             <h2 class="text-center gray-font my-4">Ready to get your needs solved?</h2>
+             <div class="container CTA-box-container">
+                 <div class="row">
+                     <div class="col-12 col-lg-6 CTA-box CTA-box-white ">
+                         <div class="CTA-box-content-container mb-lg-3 mt-lg-3">
+                            <div>
+                                <!-- <img class="img-fluid" src="./images/pages/landing/CTABottomHomeowner.jpg" alt="Homeowner using the Homehero app"> -->
+                            </div>
+                            <div class="CTA-button-container">
+                                <button class="btn btn-warning btn-lg text-white font-weight-bold w-100 mb-2">
+                                    Sign Up
+                                </button>
+                                <p>Get your FREE online account and start looking for a Hero to help with your home.</p>
+                            </div>
+                         </div>
+                     </div>
+                     <div class="col-12 col-lg-6 CTA-box CTA-box-offwhite">
+                        <div class="CTA-box-content-container mb-lg-3 mt-lg-3">
+                            <div>
+                                <!-- <img class="img-fluid" src="./images/pages/landing/CTABottomWorker.jpg" alt="Worker using the Homehero app"> -->
+                            </div>
+                            <div class="CTA-button-container">
+                                <button class="btn btn-warning btn-lg text-white font-weight-bold w-100 mb-2"">
+                                    Become a HomeHero
+                                </button>
+                                <p>Register as a HomeHero and easily be able to find and connect to clients.</p>
+                            </div>
+                        </div>
+                    </div>
+                 </div>
+             </div>
+        </div>
+    </div>
+      
 
 <?php 
-    require_once dirname(__FILE__)."/$level/components/footer.php"; 
+    require_once dirname(__FILE__)."/$level/./components/footer.php"; 
 ?>
+
+
 
     <!-- === Your Custom Page Content Goes Here above here === -->
     </div>
 <?php require_once dirname(__FILE__)."/$level/components/foot-meta.php"; ?>
 <!-- Custom JS Scripts Below -->
-    <!-- <script src="../../js/pages/user-home.js"></script> -->
-    <script>
-
-   
-
-    </script>
+    <script src="./js/pages/landing.js"></script>
 </body>
 </html>
