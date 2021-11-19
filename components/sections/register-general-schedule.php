f9c64675be47618e9c3959cf445ec793be6c8cda
marescanog <marescanog@gmail.com>
2021-11-19 21:04:57 +0800

    added time conversions for schedule

---
+++ components/sections/register-general-schedule.php
@@ -0,0 +1,42 @@
+<?php
+    $daysOfTheWeek = [
+        "Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"
+    ];
+    $week = isset($_POST["week"]) ? $_POST["week"] : null;
+?>
+<h6>Schedule Preference</h6>
+<div class="card">
+  <div class="card-body">
+      <?php 
+        for($x=0; $x<7; $x++ ){
+      ?>
+      <div class="row">
+        <div class="col-4">
+            <p><?php echo $daysOfTheWeek[$x];?></p>
+        </div>
+        <div class="col-8">
+            <p class="text-right">
+                <?php
+                    $dayObj = ($week == null) ? null :  $week[$x];;
+                    if($week != null){
+                        if($dayObj["isDayOff"]){
+                            echo "Day off";
+                        }else{
+                            echo $dayObj["start"]." - ".$dayObj["end"];
+                        }
+                    } else {
+                        echo "9:00 AM - 5:00 PM";
+                    }
+                ?>
+            </p>
+        </div>
+      </div>
+      <?php 
+        }
+      ?>
+      <div class="row">
+          <p class="text-center col-12 m-0 p-0">Edit Times</p>
+      </div>
+  </div>
+</div>
+<p class="mt-3 mb-0 p-0">* You'll still be able to change your schedule through your online account settings.</p>
--- js/pages/worker-register.js
+++ js/pages/worker-register.js
@@ -28,6 +28,7 @@ $(document).ready(()=>{
     loadRegisterBody[page]();
 })
 
+// Page 1
 const loadOrientation = () => {
     const level = getDocumentLevel();
     data={}
@@ -35,62 +36,120 @@ const loadOrientation = () => {
     $("#body").load(level+"/components/sections/register-orientation.php", data, ()=>{
         const next = document.getElementById("next");
         next.addEventListener("click", ()=>{
-            window.location.href = getDocumentLevel()+"/pages/worker/register.php"+"?page=1";
+            window.location.href = level+"/pages/worker/register.php"+"?page=1";
         })
     });
 }
 
+// Page 2
 const loadPersonalInfo = () => {
     const level = getDocumentLevel();
     $("#body").load(level+"/components/sections/register-personal-info.php", ()=>{
         const next = document.getElementById("next");
         const back = document.getElementById("back");
         next.addEventListener("click", ()=>{
-            window.location.href = getDocumentLevel()+"/pages/worker/register.php"+"?page=2";
+            window.location.href = level+"/pages/worker/register.php"+"?page=2";
         })
         back.addEventListener("click", ()=>{
-            window.location.href = getDocumentLevel()+"/pages/worker/register.php";
+            window.location.href = level+"/pages/worker/register.php";
         })
     });
 }
 
+// Page 3
 const loadSchedule = () => {
     const level = getDocumentLevel();
     $("#body").load(level+"/components/sections/register-schedule.php", ()=>{
         const next = document.getElementById("next");
         const back = document.getElementById("back");
         next.addEventListener("click", ()=>{
-            window.location.href = getDocumentLevel()+"/pages/worker/register.php"+"?page=3";
+            window.location.href = level+"/pages/worker/register.php"+"?page=3";
         })
         back.addEventListener("click", ()=>{
-            window.location.href = getDocumentLevel()+"/pages/worker/register.php"+"?page=1";
+            window.location.href = level+"/pages/worker/register.php"+"?page=1";
         })
+
+        const rawPretendData = {
+            "id": "1",
+            "is_monday_off": "0",
+            "monday_start_time": "09:00:00", 
+            "monday_end_time": "17:00:00",
+            "is_tuesday_off": "0",
+            "tuesday_start_time": "09:00:00", 
+            "tuesday_end_time": "17:00:00",
+            "is_wednesday_off": "0",
+            "wednesday_start_time": "09:00:00", 
+            "wednesday_end_time": "17:00:00",
+            "is_thursday_off": "0",
+            "thursday_start_time": "09:00:00", 
+            "thursday_end_time": "17:00:00",
+            "is_friday_off": "0",
+            "friday_start_time": "09:00:00", 
+            "friday_end_time": "17:00:00",
+            "is_saturday_off": "1",
+            "saturday_start_time": "09:00:00", 
+            "saturday_end_time": "17:00:00",
+            "is_sunday_off": "1",
+            "sunday_start_time": "09:00:00", 
+            "sunday_end_time": "17:00:00"
+        }
+
+        // Convert Raw Data to an array
+        const arrayData = Object.values(rawPretendData);
+        const scheduleData = {};
+        const week = [];
+
+        // Fomat the array to an array of objects
+        for(x=1; x<arrayData.length;x+=3){
+            const day = {};
+            day["isDayOff"] = arrayData[x];
+            day["start"] = convertFrom24To12Format(arrayData[x+1]);
+            day["end"] = convertFrom24To12Format(arrayData[x+2]);
+            week.push(day);
+        }
+       
+        // Add the new array as data to be passed into
+        scheduleData["week"] = week;
+
+        $("#schedule-preference").load(level+"/components/sections/register-general-schedule.php",scheduleData,()=>{
+
+        });
     });
 }
 
+const convertFrom24To12Format = (time24) => {
+    const [sHours, minutes] = time24.match(/([0-9]{1,2}):([0-9]{2})/).slice(1);
+    const period = +sHours < 12 ? 'AM' : 'PM';
+    const hours = +sHours % 12 || 12;
+  
+    return `${hours}:${minutes} ${period}`;
+}
+
+// Page 3
 const loadServiceArea = () => {
     const level = getDocumentLevel();
     $("#body").load(level+"/components/sections/register-service-area.php", ()=>{
         const next = document.getElementById("next");
         const back = document.getElementById("back");
         next.addEventListener("click", ()=>{
-            window.location.href = getDocumentLevel()+"/pages/worker/register.php"+"?page=4";
+            window.location.href = level+"/pages/worker/register.php"+"?page=4";
         })
         back.addEventListener("click", ()=>{
-            window.location.href = getDocumentLevel()+"/pages/worker/register.php"+"?page=2";
+            window.location.href = level+"/pages/worker/register.php"+"?page=2";
         })
     });
 }
 
+// Page 4
 const loadReview = () => {
     const level = getDocumentLevel();
     $("#body").load(level+"/components/sections/register-review.php", ()=>{
         const next = document.getElementById("next");
         next.addEventListener("click", ()=>{
-            window.location.href = getDocumentLevel()+"/pages/worker/completed-registration.php";
+            window.location.href = level+"/pages/worker/completed-registration.php";
         })
         back.addEventListener("click", ()=>{
-            window.location.href = getDocumentLevel()+"/pages/worker/register.php"+"?page=3";
+            window.location.href = level+"/pages/worker/register.php"+"?page=3";
         })
     });
 }
