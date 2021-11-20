153ddf46327aa7f4d3d4c50f91273918eca95ca5
marescanog <marescanog@gmail.com>
2021-11-21 06:26:29 +0800

    added map logic

--- components/sections/register-service-area.php
+++ components/sections/register-service-area.php
@@ -1,3 +1,6 @@
+<?php
+    $cities = ["Bantayan", "Carcar", "Cebu City", "Daanbantayan", "Danao", "Lapu-lapu", "Liloan", "Mandaue", "Minglanilla", "Naga", "Talisay", "Toledo"];
+?>
 <div class="row d-flex flex-column title-2-container pt-1 pt-lg-3">
     <h2 class="title-style-2">Set your preferred service areas</h2>
     <h6 class="title-subtitle-1">Customers will be able to find you based on where you choose to work.</h6>
@@ -5,8 +8,50 @@
 <form>
 <div class="row card-container">
     <div class="card sm-shadow">
-        <div class="card-body">
+        <div class="card-body min-card-cities">
+            <h6>Preferred cities</h6>
+            <p>Select the cities you would like to serve</p>
+            <p class="clicky"></p>
+            <div class="row">
+                <div class="col-6">
+                    <?php 
+                        for($x=0; $x<6; $x++){
+                    ?>
+                    <div class="pt-0 pt-lg-2">
+                        <div class="custom-control custom-checkbox pt-3">
+                            <input type="checkbox" class="custom-control-input" id="chk-<?php echo htmlentities($cities[$x]);?>" name="chk-<?php echo $daysOfTheWeek[$x];?>">
+                            <label class="custom-control-label check-label" for="chk-<?php echo htmlentities($cities[$x]);?>">
+                                <?php echo htmlentities($cities[$x]);?>
+                            </label>
+                        </div>
+                    </div>
+                    <?php 
+                        }
+                    ?>
+                </div>
 
+                <div class="col-6">
+                    <?php 
+                        for($x=6; $x<12; $x++){
+                    ?>
+                    <div class="col-lg-2 pt-0 pt-lg-2">
+                        <div class="custom-control custom-checkbox pt-3">
+                            <input type="checkbox" class="custom-control-input" id="chk-<?php echo htmlentities($cities[$x]);?>" name="chk-<?php echo $daysOfTheWeek[$x];?>">
+                            <label class="custom-control-label check-label" for="chk-<?php echo htmlentities($cities[$x]);?>">
+                                <?php echo htmlentities($cities[$x]);?>
+                            </label>
+                        </div>
+                    </div>
+                    <?php 
+                        }
+                    ?>
+                </div>
+            </div>
+            <hr>
+            <div>
+                <div id="map"></div>
+            </div>
+            <p class="mt-3 mb-0 p-0 note-bottom smol">* You'll still be able to modify your preferences through your online account settings.</p>
         </div>
     </div>
 </div>
@@ -19,3 +64,14 @@
     </div>
 </div>
 </form>
+
+
+<!-- Map Data -->
+<script src="../../js/helper/city_delimeters.js"></script>
+<!-- Map Code -->
+<script src="../../js/helper/map.js"></script>
+<!-- Async script executes immediately and must be after any DOM elements used in callback. -->
+<script
+    src="https://maps.googleapis.com/maps/api/js?key=ZEAPIKEY&callback=initMap&libraries=&v=weekly"
+    async
+></script>
---
+++ js/helper/map.js
@@ -0,0 +1,255 @@
+
+
+function initMap() {
+
+    // The location of Uluru
+    const cebu = { lat: 10.678, lng: 123.776 };
+    // Initialize some map with center at cebu
+    const map = new google.maps.Map(document.getElementById('map'), {
+        center: cebu,
+        zoom: 8.7,
+        mapTypeId: 'roadmap',
+        disableDefaultUI: true
+    });
+
+    const BantayanPolygons = bantayan_Delimeters.map(delimeter => {
+        return new google.maps.Polygon({
+        paths: delimeter,
+        strokeColor: '#890A98',
+        strokeOpacity: 0.8,
+        strokeWeight: 3,
+        fillColor: '#A708F5',
+        fillOpacity: 0.35
+        });
+    });
+
+    var CebuPolygon = new google.maps.Polygon({
+        paths: cebu_Delimeters,
+        strokeColor: '#890A98',
+        strokeOpacity: 0.8,
+        strokeWeight: 3,
+        fillColor: '#A708F5',
+        fillOpacity: 0.35
+    });
+    
+    var Carcar_Polygon = new google.maps.Polygon({
+        paths: carcar_Delimeters,
+        strokeColor: '#890A98',
+        strokeOpacity: 0.8,
+        strokeWeight: 3,
+        fillColor: '#A708F5',
+        fillOpacity: 0.35
+    });
+   
+    const Daanbantayan_Polygon = new google.maps.Polygon({
+        paths: daanbantayan_Delimeters,
+        strokeColor: '#890A98',
+        strokeOpacity: 0.8,
+        strokeWeight: 3,
+        fillColor: '#A708F5',
+        fillOpacity: 0.35
+    });
+
+    var Danao_Polygon = new google.maps.Polygon({
+        paths: danao_Delimeters,
+        strokeColor: '#890A98',
+        strokeOpacity: 0.8,
+        strokeWeight: 3,
+        fillColor: '#A708F5',
+        fillOpacity: 0.35
+    });
+    
+    const LapulapuPolygons = lapulapu_Delimeters.map(delimeter => {
+        return new google.maps.Polygon({
+        paths: delimeter,
+        strokeColor: '#890A98',
+        strokeOpacity: 0.8,
+        strokeWeight: 3,
+        fillColor: '#A708F5',
+        fillOpacity: 0.35
+        });
+    });
+    
+    var Liloan_Polygon = new google.maps.Polygon({
+        paths: liloan_Delimeters,
+        strokeColor: '#890A98',
+        strokeOpacity: 0.8,
+        strokeWeight: 3,
+        fillColor: '#A708F5',
+        fillOpacity: 0.35
+    });
+
+     var Mandaue_Polygon = new google.maps.Polygon({
+        paths: mandaue_Delimeters,
+        strokeColor: '#890A98',
+        strokeOpacity: 0.8,
+        strokeWeight: 3,
+        fillColor: '#A708F5',
+        fillOpacity: 0.35
+    });
+    
+     var Minglanilla_Polygon = new google.maps.Polygon({
+        paths: minglanilla_Delimeters,
+        strokeColor: '#890A98',
+        strokeOpacity: 0.8,
+        strokeWeight: 3,
+        fillColor: '#A708F5',
+        fillOpacity: 0.35
+    });
+    
+    var Naga_Polygon = new google.maps.Polygon({
+        paths: naga_Delimeters,
+        strokeColor: '#890A98',
+        strokeOpacity: 0.8,
+        strokeWeight: 3,
+        fillColor: '#A708F5',
+        fillOpacity: 0.35
+    });
+    
+    var Talisay_Polygon = new google.maps.Polygon({
+        paths: talisay_Delimeters,
+        strokeColor: '#890A98',
+        strokeOpacity: 0.8,
+        strokeWeight: 3,
+        fillColor: '#A708F5',
+        fillOpacity: 0.35
+    });
+    
+    var Toledo_Polygon = new google.maps.Polygon({
+        paths: toledo_Delimeters,
+        strokeColor: '#890A98',
+        strokeOpacity: 0.8,
+        strokeWeight: 3,
+        fillColor: '#A708F5',
+        fillOpacity: 0.35
+    });
+
+
+    const setBantayan =()=> {
+        BantayanPolygons.forEach(poly=>{
+            poly.setMap(map);
+        });
+    };
+
+    const unsetBantayan =()=> {
+        BantayanPolygons.forEach(poly=>{
+            poly.setMap(null);
+        });
+    };
+
+    const setCarcar =()=>{
+        Carcar_Polygon.setMap(map);
+    };
+
+    const unsetCarcar =()=>{
+        Carcar_Polygon.setMap(null);
+    };
+
+    const setCebu =()=>{
+        CebuPolygon.setMap(map);
+    };
+
+    const unsetCebu =()=>{
+        CebuPolygon.setMap(null);
+    };
+
+    const setDaanbantayan =()=>{
+        Daanbantayan_Polygon.setMap(map);
+    };
+
+    const unsetDaanbantayan =()=>{
+        Daanbantayan_Polygon.setMap(null);
+    };
+
+    const setDanao =()=>{
+        Danao_Polygon.setMap(map);
+    };
+
+    const unsetDanao =()=>{
+        Danao_Polygon.setMap(null);
+    };
+
+    const setLapuLapu =()=> {
+        LapulapuPolygons.forEach(poly=>{
+            poly.setMap(map);
+        });
+    };
+
+    const unsetLapuLapu =()=> {
+        LapulapuPolygons.forEach(poly=>{
+            poly.setMap(null);
+        });
+    };
+
+    const setLiloan =()=>{
+        Liloan_Polygon.setMap(map);
+    };
+
+    const unsetLiloan =()=>{
+        Liloan_Polygon.setMap(null);
+    };
+
+    const setMandaue =()=>{
+        Mandaue_Polygon.setMap(map);
+    };
+
+    const unsetMandaue =()=>{
+        Mandaue_Polygon.setMap(null);
+    };
+
+    const setMinglanilla =()=>{
+        Minglanilla_Polygon.setMap(map);
+    };
+
+    const unsetMinglanilla =()=>{
+        Minglanilla_Polygon.setMap(null);
+    };
+
+    const setNaga =()=>{
+        Naga_Polygon.setMap(map);
+    };
+
+    const unsetNaga =()=>{
+        Naga_Polygon.setMap(null);
+    };
+
+    const setTalisay =()=>{
+        Talisay_Polygon.setMap(map);
+    };
+
+    const unsetTalisay =()=>{
+        Talisay_Polygon.setMap(null);
+    };
+
+    const setToledo =()=>{
+        Toledo_Polygon.setMap(map);
+    };
+
+    const unsetToledo =()=>{
+        Toledo_Polygon.setMap(null);
+    };
+
+    const setPolygons = [
+        setBantayan, setCarcar, setCebu, setDaanbantayan, setDanao, setLapuLapu, setLiloan, setMandaue, setMinglanilla, setNaga, setTalisay, setToledo
+    ];
+
+    const unsetPolygons = [
+        unsetBantayan, unsetCarcar, unsetCebu, unsetDaanbantayan, unsetDanao, unsetLapuLapu, unsetLiloan, unsetMandaue, unsetMinglanilla, unsetNaga, unsetTalisay, unsetToledo
+    ];
+
+    const cityNames = ["Bantayan", "Carcar", "Cebu City", "Daanbantayan", "Danao", "Lapu-lapu", "Liloan", "Mandaue", "Minglanilla", "Naga", "Talisay", "Toledo"];
+
+    for(let x = 0; x < cityNames.length; x++){
+        const check = document.getElementById("chk-"+cityNames[x]);
+
+        check.addEventListener("click", ()=>{
+            if(check.checked){
+                setPolygons[x]();
+            } else {
+                unsetPolygons[x]();
+            }
+        });
+    }
+
+
+};
