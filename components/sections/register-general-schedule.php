d97e3880ea8ef7e5afa135b2ab9b071236f27ab2
marescanog <marescanog@gmail.com>
2021-11-28 18:13:18 +0800

    set up the skeleton for the curl request for schedules

--- components/sections/register-general-schedule.php
+++ components/sections/register-general-schedule.php
@@ -1,10 +1,72 @@
 <?php
+
+session_start();
+
+// // Make curl for the personal inforation pagge information vv
+// // $url = "http://localhost/slim3homeheroapi/public/registration/personal-info"; // DEV
+// // $url = "https://slim3api.herokuapp.com/registration/personal-info"; // PROD
+// $post_data = array(
+//     'query' => 'some stuff',
+//     'method' => 'post',
+//     'ya' => 'boo'
+// );
+
+// $headers = array(
+//     "Authorization: Bearer ".$_SESSION["registration_token"],
+//     'Content-Type: application/json',
+// );
+
+// // 1. Initialize
+// $ch = curl_init();
+
+// // 2. set options
+//     // URL to submit to
+//     curl_setopt($ch, CURLOPT_URL, $url);
+
+//     // Return output instead of outputting it
+//     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+//     // Type of request = POST
+//     // curl_setopt($ch, CURLOPT_POST, 1);
+//     curl_setopt($ch, CURLOPT_HTTPGET, 1);
+
+//     // Set headers for auth
+//     curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+    
+//     // Adding the post variables to the request
+//     // curl_setopt($ch, CURLOPT_POSTFIELDS, $post_data);
+
+//     // Execute the request and fetch the response. Check for errors
+//     $output = curl_exec($ch);
+
+//     if($output === FALSE){
+//         echo "cURL Error:" . curl_error($ch);
+//     }
+
+//     curl_close($ch);
+
+//     // $output =  json_decode(json_encode($output), true);
+//     $output =  json_decode($output);
+
+    // =================================
+    // =================================
+
+    // Data for rendering
     $daysOfTheWeek = [
         "Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"
     ];
     $week = isset($_POST["week"]) ? $_POST["week"] : null;
     $isOnEdit = isset($_GET["edit"]) ? $_GET["edit"] : false;
+
+    // =================================
+    // =================================
+    // Data from User, format & clean
 ?>
+<?php  //echo var_dump($output);?>
+<?php  echo var_dump($_SESSION["registration_token"]);?>
+
+
+
 <h6>Schedule Preference</h6>
 <div class="row ml-3 mb-2">
     <div class="radio-item col-12 m-0">
