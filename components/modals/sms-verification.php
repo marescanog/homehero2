eef6c018c0085f8d0f229656ebec02dd5d510eaf
marescanog <marescanog@gmail.com>
2021-11-30 09:41:18 +0800

    added edits to the SMS modal to reflect 6 digits and for data storage

--- components/modals/sms-verification.php
+++ components/modals/sms-verification.php
@@ -1,11 +1,22 @@
-<!-- 
-    To be inserted:
-        Form onsubmit event
-        Dynamic phone number implementation
-
--->
 <?php 
-    $level = isset($_POST['level']) ? $_POST['level'] : '.';
+    // on the SMS verification worker form, write PHP to grab the POST data and echo it back into a hidden form
+    // Once SMS verificaiton submission is successful, ajax to write to database on the js for the modal sms verification
+
+    // Grab information passed from Worker registration form
+    $level = isset($_POST['level']) ? $_POST['level'] : '../..';
+    $firstName = null;
+    $lastName = null;
+    $phone = null;
+    $securepass = null;
+    $data = null;
+    if(isset($_POST['data'])){
+        $data = $_POST['data'];
+        $firstName = isset($data['first_name']) ? $data['first_name'] : null;
+        $lastName = isset($data['last_name']) ? $data['last_name'] : null;
+        $phone = isset($data['phone']) ? $data['phone'] : null;
+        $securepass = isset($data['securepass']) ? $data['securepass'] : null;
+        $messagebirdID = isset($data['messagebird_id']) ? $data['messagebird_id'] : null;
+    }
 ?>
 <div id="sms-modal" class="modal-content">
     <div class="modal-header">
@@ -17,11 +28,7 @@
         </button>
     </div>
     <div class="modal-body">
-        <p>
-            <?php echo var_dump($_POST);
-            ?>
-        </p>
-        <form id="SMSVerification" type="POST" onSubmit="SMSVerificationHandler(event)" name="modalForm" class="m-4">
+        <form id="SMSVerification" type="POST" name="modalForm" class="m-4">
         <h5 style="font-family: Segoe UI;
                 font-style: normal;
                 font-weight: bold;
@@ -29,56 +36,43 @@
                 line-height: 24px;
                 color: #707070;
         ">Verify SMS</h5>
-        <p>Enter the verification PIN sent to <?php echo "0912 345 6789" ?></p>
-            <div class="d-flex" style="height:200px;">
-                <input name="code-1" class="mr-1 text-center code" type="text" 
+        <p>Enter the verification PIN sent to <?php echo htmlentities($phone); ?></p>
+        <div class="d-flex flex-column" style="height:200px;">
+            <div class="d-flex form-sms" >
+                <input name="code-1" class="mr-1 text-center code" type="tel" 
                     style="width:30px; height:43px; font-size:2rem; outline:0; border-width:0px 0px 2px; overflow:hidden" placeholder="0" required maxlength="1">
-                <input name="code-2" class="mr-1 text-center code" type="text"
+                <input name="code-2" class="mr-1 text-center code" type="tel" 
                     style="width:30px; height:43px; font-size:2rem; outline:0; border-width:0px 0px 2px; overflow:hidden" placeholder="0" required maxlength="1">
-                <input name="code-3" class="mr-1 text-center code" type="text"
+                <input name="code-3" class="mr-1 text-center code" type="tel" 
                     style="width:30px; height:43px; font-size:2rem; outline:0; border-width:0px 0px 2px; overflow:hidden" placeholder="0" required maxlength="1">
-                <input name="code-4" class="mr-1 text-center code" type="text"
+                <input name="code-4" class="mr-1 ml-4 text-center code" type="tel" 
                     style="width:30px; height:43px; font-size:2rem; outline:0; border-width:0px 0px 2px; overflow:hidden" placeholder="0" required maxlength="1">
+                    <input name="code-5" class="mr-1 text-center code" type="tel" 
+                    style="width:30px; height:43px; font-size:2rem; outline:0; border-width:0px 0px 2px; overflow:hidden" placeholder="0" required maxlength="1">
+                <input name="code-6" class="mr-1 text-center code" type="tel" 
+                    style="width:30px; height:43px; font-size:2rem; outline:0; border-width:0px 0px 2px; overflow:hidden" placeholder="0" required maxlength="1">
+                <input type="hidden" name="messagebird_id" value="<?php echo htmlentities($messagebirdID); ?>">
             </div>
-            <button type="submit" class="btn btn-warning text-white font-weight-bold w-100">
-                <span id="RU-submit-btn-txt">VERIFY ACCOUNT</span>
-                <div id="RU-submit-btn-load" class="d-none">
+            <div id="sms-error-display" class="c d-none">
+                <p class="text-danger">Your verification PIN is required</p>
+            </div>
+        </div>
+            <button id="WSMS-submit-btn" type="submit" class="btn btn-warning text-white font-weight-bold w-100">
+                <span id="WSMS-submit-btn-txt">VERIFY ACCOUNT</span>
+                <div id="WSMS-submit-btn-load" class="d-none">
                     <span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
                     <span class="sr-only">Loading...</span>
                 </div>
             </button>
         </form>
+        <form id="formData" type="POST">
+            <input type="hidden" name="first_name" value="<?php echo htmlentities($firstName);?>" readonly>
+            <input type="hidden" name="last_name" value="<?php echo htmlentities($lastName);?>" readonly>
+            <input type="hidden" name="phone" value="<?php echo htmlentities($phone);?>" readonly>
+            <input type="hidden" name="pass" value="<?php echo htmlentities($securepass);?>" readonly>
+        </form>
     </div>
 
 </div>
-
-<!-- 
-    Script to limit input to numbers and enable auto focus change for input once a number is inputted 
-    Source: https://stackoverflow.com/questions/10539113/focusing-on-next-input-jquery
--->
-<script>
-$(function() {
-    var charLimit = 1;
-    $(".code").keydown(function(e) {
-
-        var keys = [8, 9, /*16, 17, 18,*/ 19, 20, 27, 33, 34, 35, 36, 37, 38, 39, 40, 45, 46, 144, 145];
-
-        if (e.which == 8 && this.value.length == 0) {
-            $(this).prev('.code').focus();
-        } else if ($.inArray(e.which, keys) >= 0) {
-            return true;
-        } else if (this.value.length >= charLimit) {
-            $(this).next('.code').focus();
-            return false;
-        } else if (e.shiftKey || e.which <= 48 || e.which >= 58) {
-            return false;
-        }
-    }).keyup (function () {
-        if (this.value.length >= charLimit) {
-            $(this).next('.code').focus();
-            return false;
-        }
-    });
-});
-</script>
+<script src="<?php echo $level?>/js/components/modal-validation/modal-homeowner-sms.js"></script>
 
