b87ace037783e4c86061a20822f08da7ab64401d
marescanog <marescanog@gmail.com>
2021-11-30 09:40:19 +0800

    added js script and adjusted variable names to fit data passing

--- components/modals/homeowner-register.php
+++ components/modals/homeowner-register.php
@@ -1,11 +1,6 @@
 <!-- 
-    Merged some contents such as ids and other properties
     To be inserted:
-        Links for terms and conditions, privacy policy, login, and worker registration anchors
-
-    Additional Note: Since this modal will be used in different pages with different 
-        folder addresses, Include a php call out of level.
-        Validation will now be manual JS since we will base it on form submisstion.
+        worker registration anchors
  -->
 <?php 
     $level = isset($_POST['level']) ? $_POST['level'] : '.';
@@ -21,29 +16,33 @@
     </div>
     <div class="modal-body">
         <h5 class="font-weight-bold" style="color: #707070;">Homeowner Sign-Up</h5>
-        <form id="registerForm" type="POST" onSubmit="registerHandler(event)" name="modalForm" class="m-4">
+        <form id="registerForm" type="POST" name="modalForm" class="m-4">
+            <!-- First Name and Last Name Input Feilds -->
             <div class="d-flex">
-                <div class="form-group w-100 mr-2">
-                    <input type="text" class="form-control" id="RU_firstName" name="first_name" placeholder="First Name" autocomplete required maxlength="50">
+                <div class="form-group w-100 mr-1">
+                    <input id="RU_firstname" class="form-control" name="first_name" minlength="2" placeholder="First Name" type="text" maxlength="51" autocomplete required>
                 </div>
-                <div class="form-group w-100 ml-2">
-                    <input type="text" class="form-control" id="RU_lastName" name="last_name" placeholder="Last Name" autocomplete required maxlength="50">
+                <div class="form-group w-100 ml-1">
+                    <input id="RU_lastname" class="form-control" name="last_name" minlength="2" placeholder="Last Name" type="text" maxlength="51" autocomplete required>
                 </div>
             </div>
+            <!-- Mobile Number input feild -->
+            <div id="RU_phone_formGroup" class="form-group">
+                <input type="text" class="form-control" id="RU_phone" name="phone" placeholder="Mobile number (09XXXXXXXXX)" autocomplete required maxlength="15">
+            </div>
+            <!-- Password and Confirm password input feild -->
             <div class="form-group">
-                <input type="text" class="form-control" id="RU_phone" name="phone_number" placeholder="Mobile number (09XXXXXXXXX)" autocomplete required maxlength="15">
+                <input type="password" class="form-control" id="password" name="password" placeholder="Enter password" minlength="8" maxlength="31" autocomplete >
             </div>
             <div class="form-group">
-                <input type="password" class="form-control" id="RU_password" name="password" placeholder="Enter password" autocomplete required maxlength="50">
+                <input type="password" class="form-control" id="confirm_password" name="confirm_password" placeholder="Re-enter password" autocomplete >
             </div>
-            <div class="form-group">
-                <input type="password" class="form-control" id="RU_confirmPassword" name="confirm_password" placeholder="Re-enter password" autocomplete required maxlength="50">
-            </div>
+            <!-- Checkbox  -->
             <div class="form-group form-check">
-                <input type="checkbox" class="form-check-input" id="agree" required>
+                <input type="checkbox" class="form-check-input" id="agree" name="agree" required>
                 <label class="form-check-label" for="agree" style="font-size:0.8em;">I agree to HomeHero's <a href="#">Terms and Conditions</a> and <a href="#">Privacy Policy</a>.</label>
             </div>
-            <button id="RU-submit-btn" type="button" class="btn btn-warning text-white font-weight-bold w-100 mb-3">
+            <button id="RU-submit-btn"  type="submit" value="Submit"  class="btn btn-warning text-white font-weight-bold w-100 mb-3">
                 <span id="RU-submit-btn-txt">CREATE ACCOUNT</span>
                 <div id="RU-submit-btn-load" class="d-none">
                     <span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
@@ -61,3 +60,4 @@
         </form>
     </div>
 </div>
+<script src="<?php echo $level?>/js/components/modal-validation/modal-homeowner-register.js"></script>
