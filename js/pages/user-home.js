66bb7bad54e3d35ce612c89fc479158580d3ac02
marescanog <marescanog@gmail.com>
2021-12-01 20:27:27 +0800

    added user home functionality and jquery ui for search

--- js/pages/user-home.js
+++ js/pages/user-home.js
@@ -1,21 +1,77 @@
-// $(document).ready(()=>{
-//     var form = document.getElementById("form-home");
-//     var button = document.getElementById("button-home");
+$(document).ready(()=>{
 
-//     // console.log(form);
-//     // console.log(button);
+    const addAddress = document.getElementById("add-address");
+    const modal = document.getElementById("modal");
+    const getStarted = document.getElementById("button-home");
+    const myform = document.getElementById("form-home");
 
-//     form.setAttribute("onSubmit", "submitForm(event)");
+    addAddress.addEventListener("click", ()=>{
+        console.log("click");
+        loadModal("tempAddr",modalTypes, ()=>{}, getDocumentLevel());
+        $("#modal").modal("show");
+    });
 
-//     button.addEventListener("click", ()=>{
-//         console.log("button has been clicked");  
-//         // const modalTypes = {
-//         // "login" : "../../components/modals/temp-enter-address.php",
-//         // "error" : "../../components/modals/error.php" 
-//         // }
-//         // loadModal("login", modalTypes);
-//     });
-// })
+    getStarted.addEventListener("click", ()=>{
+        // console.log(addAddress)
+        // console.log(modal)
+        // console.log(getStarted)
+        // console.log(myform)
+
+        // Grab the feilds to validate and the UI to display error messages
+        const proj_id_field = document.getElementById("project_id_field");
+        const proj_cat_field = document.getElementById("project_category_field");
+        const home_add_field = document.getElementById("home_address_field");
+        const myform = document.getElementById("form-home");
+        const errorMessageDisplay = document.getElementById("form-err-msg");
+
+        let errorMessage = "";
+        let isError = false;
+        // classes are error and d-none
+
+        if (proj_id_field.value == "" && home_add_field.value == ""){
+            errorMessage = "* Please select a project from the list. Please add an address for the project.";
+            isError = true;
+        }
+
+        else if (proj_id_field.value == "" ){
+            errorMessage = "* Please select a project from the list.";
+            isError = true;
+        }
+
+        else if (home_add_field.value == ""){
+            errorMessage = "* Please add an address for the project.";
+            isError = true;
+        }
+
+        else {
+            isError = false;
+        }
+
+        if(isError){
+            if(! $("#form-home").hasClass("error")){
+                $("#form-home").addClass("error");
+            } 
+            if($("#form-err-msg").hasClass("d-none")){
+                $("#form-err-msg").removeClass("d-none");
+            } 
+        } else {
+            if( $("#form-home").hasClass("error")){
+                $("#form-home").removeClass("error");
+            } 
+            if(! $("#form-err-msg").hasClass("d-none")){
+                $("#form-err-msg").addClass("d-none");
+            } 
+            console.log("Submitting form///")
+            myform.submit();
+            //window.location.href = getDocumentLevel()+"/pages/homeowner/create-project.php";
+        }
+
+
+
+    });
+
+    
+})
 
 // const submitForm = (e)=>{
 //     e.preventDefault();
