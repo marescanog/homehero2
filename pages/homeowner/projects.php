adab5ec5d3fc281d2fb7a11b9cf946d5fc827eaa
marescanog <marescanog@gmail.com>
2021-12-03 04:39:28 +0800

    modified the projects page

--- pages/homeowner/projects.php
+++ pages/homeowner/projects.php
@@ -34,47 +34,46 @@ require_once dirname(__FILE__)."/$level/components/head-meta.php";
         <div class="h-100">
             <nav aria-label="breadcrumb">
                 <ol class="breadcrumb mx-2 mx-lg">
-                    <li class="breadcrumb-item"><a href="#">Home</a></li>
-                    <li class="breadcrumb-item"><a href="#">Library</a></li>
-                    <li class="breadcrumb-item active" aria-current="page">Data</li>
+                    <li class="breadcrumb-item" aria-current="page"><a href="#">All projects</a></li>
                 </ol>
             </nav>
             <div class="mt-0 mb-2 d-flex align-items-center justify-content-between">
                 <div>
-                    <h1 class="h3 mx-2 mx-lg-0 mt-0 mb-2">Plumbing Repair Project</h1>
+                    <h1 class="h3 mx-2 mx-lg-0 mt-0 mb-2">Your Projects</h1>
                 </div>
-                <div class="sidelink">
+                <!-- <div class="sidelink">
                     <p class="mt-3 mr-3 mr-lg-0 text-danger">CANCEL</p>
-                </div>
+                </div> -->
             </div>
         </div>
         <div class="h-100">
             <div  id="tabs" class="card-body">
                 <nav>
                     <div class="nav nav-tabs" id="nav-tab" role="tablist">
-                        <p class="nav-item nav-link active " id="nav-hire-tab" data-toggle="tab" href="#nav-hire" role="tab" aria-controls="nav-hire" aria-selected="true">Hire a Hero</p>
-                        <p class="nav-item nav-link" id="nav-work-tab" data-toggle="tab" href="#nav-work" role="tab" aria-controls="nav-work" aria-selected="false">Find Work</p>
+                        <p class="nav-item nav-link active " id="nav-hire-tab" data-toggle="tab" href="#nav-hire" role="tab" aria-controls="nav-hire" aria-selected="true">Ongoing Projects</p>
+                        <p class="nav-item nav-link" id="nav-work-tab" data-toggle="tab" href="#nav-work" role="tab" aria-controls="nav-work" aria-selected="false">Closed Projects</p>
                     </div>
                 </nav>
                 <div class="tab-content pt-1 pb-2 px-2  px-lg-3" id="nav-tabContent">
                     <div class="tab-pane fade show active" id="nav-hire" role="tabpanel" aria-labelledby="nav-hire-tab">
-                        <h1 class="jumbotron-h1 mt-lg-3 mt-0 mt-md-3 mt-lg-0">
-                            Find a Hero to help improve your home.
-                        </h1>
+                        <h5 class="jumbotron-h1 mt-lg-3 mt-0 mt-md-3 mt-lg-0">
+                            You have no projects.
+                        </h5>
                     </div>
                     <div class="tab-pane fade" id="nav-work" role="tabpanel" aria-labelledby="nav-work-tab">
-                        <h1 class="jumbotron-h1 mt-lg-3">
-                            Find clients and grow your income.
-                        </h1>
+                        <h5 class="jumbotron-h1 mt-lg-3">
+                            You have no projects.
+                        </h5>
   
                     </div>
                 </div>
             </div>
         </div>
-        <div class="separator"></div>
+        <!-- <div class="separator"></div>
         <div class="mx-2 mx-lg">
-            <h4>Recommended Matches</h4>
-        </div>
+            <h4>Recommended HomeHeroes</h4>
+            <p>You have no current projects. Add a project to get recommendations!</p>
+        </div> -->
     </div>
     <!-- Footer Links -->
     <?php 
