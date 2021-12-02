6a2edac9a9cb9ed9ac199f1dc8da84d9ba52b018
marescanog <marescanog@gmail.com>
2021-12-02 17:18:49 +0800

    fixed prod api route

--- js/pages/user-create-project.js
+++ js/pages/user-create-project.js
@@ -594,7 +594,7 @@ const load_create_project_form = (
                         $.ajax({
                             type : 'POST',
                             //url : "http://localhost/slim3homeheroapi/public/add-project", // dev
-                            url : "http://localhost/slim3homeheroapi/public/add-project", // prod
+                            url : "http://localhost/slim3homeheroapi/add-project", // prod
                             data : samoka,
                                 contentType: false,
                                 processData: false,
