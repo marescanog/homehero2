1ce5a7def1bb7fa9951fc8add9522722010b74a0
marescanog <marescanog@gmail.com>
2021-12-12 09:31:00 +0800

    linked the change password route to the profile page

--- js/pages/user-profile.js
+++ js/pages/user-profile.js
@@ -32,7 +32,73 @@ $(document).ready(()=>{
             const buttonLoadSpinner = document.getElementById("CPs-submit-btn-load");
             const formData = getFormDataAsObj(form);
             disableForm_displayLoadingButton(button, buttonTxt, buttonLoadSpinner, form);
-        }
+
+            // Add token retrieval then link with change password route
+            // Ajax to get the bearer token
+            $.ajaxSetup({cache: false})
+            $.get(getDocumentLevel()+'/auth/get-register-session.php', function (data) {
+                //console.log(data)
+                const parsedSession = JSON.parse(data);
+                const token = parsedSession['token'];
+                console.log(token);
+
+                // Create new form 
+                const samoka = new FormData();
+
+                // Append information
+                samoka.append('current_pass', formData["current_pass"]);
+                samoka.append('new_pass', formData["new_pass"]);
+                samoka.append('confirm_pass', formData["confirm_pass"]);
+
+                // Ajax to save new pasword
+                $.ajax({
+                    type: 'POST',
+                    // url : '', // prod (No current deployed prod route)
+                    url: 'http://localhost/slim3homeheroapi/public/homeowner/change-password', // dev
+                    contentType: false,
+                    processData: false,
+                    headers: {
+                        "Authorization": `Bearer ${token}`
+                    },
+                    data : samoka,
+                    success : function(response) {
+                        console.log("your response after change password is:")
+                        console.log(response);
+                        Swal.fire({
+                            title: 'Password Change Success',
+                            text: 'Password has been changed',
+                            icon: 'success'
+                        });
+                        document.getElementById("profile-change-password").reset();
+                        enableForm_hideLoadingButton(button, buttonTxt, buttonLoadSpinner, form, "CHANGE");
+                    },
+                    error: function (response) {
+                        console.log(response);
+                        let res = response.responseJSON.response;
+
+                        console.log(res);
+                        if(res?.status == 400){
+                            Swal.fire({
+                                title: 'Error: Bad Request',
+                                text: res?.message,
+                                icon: 'error'
+                            });
+                        } else {
+                            Swal.fire({
+                                title: 'An error occurred',
+                                text: 'Please try again',
+                                icon: 'error'
+                            });
+                        }
+                        enableForm_hideLoadingButton(button, buttonTxt, buttonLoadSpinner, form, "CHANGE");
+
+                    }
+                });
+
+            }); //Ajax close for bearer token
+
+        } // submit handler close in Jquery
+        
     }); //JQUERY Validator Close
     
 
