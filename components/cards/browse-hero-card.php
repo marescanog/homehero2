b9ac70b9c32446cebd565d3bd7ac548d5f512919
marescanog <marescanog@gmail.com>
2021-12-12 16:27:43 +0800

    added dynamic loading in browse

--- components/cards/browse-hero-card.php
+++ components/cards/browse-hero-card.php
@@ -1,31 +1,46 @@
 
-<div class="card shadow-sm rounded mt-4">
+<div class="card shadow rounded mt-4">
   <div class="card-body">
     <div class="row">
         <div class="p-0 m-0 col-3 col-lg-2">
             <div class="avatar d-flex justify-content-center align-items-center">
-                <h2>CS</h2>
+                <h2><?php echo $winitials;?></h2>
             </div>
         </div>
         <div class="p-0 m-0 col-6 col-lg-5">
             <div class="d-flex flex-column pt-lg-3">
-                <h2>Name</h2>
+                <h2>
+                    <?php echo $name == null ? "" : htmlentities($name);?>
+                </h2>
                 <div class="d-flex align-items-center">
                     <?php 
-                        $hasRatings = true;
-                        $rating = 3;
+                        // $hasRatings = $rating_average == null ? false : true;
+                        $hasRatings = $rating_average == null ? false : true;
+                        $rating = $rating_average == null ? 0 : $rating_average;
                         include "$level/components/UX/ratingsDisplay.php";
                     ?>
-                    <h6 class="pt-1 ml-2">(6 ratings)</h6>
+                    <?php
+                        if($rating_average != null){
+                    ?>
+                    <h6 class="pt-1 ml-2">(<?php echo $total_ratings;?> ratings)</h6>
+                    <?php
+                        }
+                    ?>
                 </div>
                 <div class="d-flex align-items-center pt-1">
-                    <i class="far fa-check-circle mr-1"></i>
-                    <h6 class="p-0 m-0">6 Projects Completed</h6>
+                    <?php
+                        if($completed_jobs != 0){
+                    ?>
+                        <i class="far fa-check-circle mr-1"></i>
+                    <?php
+                        }
+                    ?>
+                    <h6 class="p-0 m-0"><?php echo $completed_jobs;?> Projects Completed</h6>
                 </div>
             </div>
         </div>
         <div class="p-0 m-0 col-2 col-lg-5 d-flex justify-content-end align-items-center">
-           <button class="btn btn-primary mr-lg-5">
+           <button id="notif-<?php echo $worker_id?>"class="btn btn-primary mr-lg-5">
                Notify
            </button>
         </div>
@@ -37,22 +52,26 @@
         <div class="col-3 justify-content-center align-items-center">
             <div class="d-flex align-items-center">
                 <i class="fas fa-map-marker-alt mr-2"></i>
-                <p class="p-0 m-0">Talisay, Mandaue</p>
+                <p class="p-0 m-0"><?php echo $city_info;?></p>
             </div>
         </div>
         <div class="col-3 justify-content-center align-items-center">
-            <div class="d-flex align-items-center">
-                <i class="fas fa-tag mr-2"></i>
-                <p class="p-0 m-0">50/hr</p>
-            </div>
+            <?php if($pricing !== null && $pricing !== "" && $pricing !== 0 && $pricing !== "0"){ ?>
+                <div class="d-flex align-items-center">
+                    <i class="fas fa-tag mr-2"></i>
+                    <p class="p-0 m-0"><?php echo $pricing;?></p>
+                </div>
+            <?php }?>
         </div>
         <div class="col-3 justify-content-center align-items-center">
+        <?php if ($skill_list != null){ ?>
             <div class="d-flex align-items-center">
                 <i class="fas fa-newspaper mr-2"></i>
                 <p class="p-0 m-0">
-                    Plumbing, Carpentry, Gardening
+                    <?php echo $skill_list;?>
                 </p>
             </div>
+        <?php } ?>
         </div>
     </div>
   </div>
--- pages/homeowner/browse.php
+++ pages/homeowner/browse.php
@@ -10,7 +10,61 @@ $level ="../..";
 $fistName = isset($_SESSION["first_name"]) ? $_SESSION["first_name"] : "Guest";
 $initials = isset($_SESSION["initials"]) ? $_SESSION["initials"] : "GU";
 
+// NODEPLOYEDPRODLINK
+// Curl request to get data to fill projects page
 
+$url = "http://localhost/slim3homeheroapi/public/homeowner/get-homeheroes"; // DEV
+// $url = ""; // NO PROD LINK
+
+// $headers = array(
+//   "Authorization: Bearer ".$_SESSION["token"],
+//   'Content-Type: application/json',
+// );
+
+// 1. Initialize
+$ch = curl_init();
+
+// 2. set options
+  // URL to submit to
+  curl_setopt($ch, CURLOPT_URL, $url);
+
+  // Return output instead of outputting it
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+  // Type of request = GET
+  curl_setopt($ch, CURLOPT_HTTPGET, 1);
+
+//   // Set headers for auth
+//   curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+  
+  // Execute the request and fetch the response. Check for errors
+  $output = curl_exec($ch);
+
+  $curl_error_message = null;
+
+  // ERROR HANDLING 
+  if($output === FALSE){
+      $curl_error_message = curl_error($ch);
+  }
+
+  curl_close($ch);
+
+  // $output =  json_decode(json_encode($output), true);
+  $output =  json_decode($output);
+  
+  // Declare variables to be used in this page
+//   $ongoingJobPosts = [];
+//   $ongoingProjects = [];
+//   $closedProjects = [];
+
+//   if(is_object($output) && $output->success == true){
+//       $ongoingJobPosts = $output->response->ongoingJobPosts;
+//       $ongoingProjects = $output->response->ongoingProjects;
+//       $closedProjects = $output->response->closedProjects;
+//   }
+
+
+// HTML STARTS HERE
 require_once dirname(__FILE__)."/$level/components/head-meta.php"; 
 
 ?>
@@ -53,22 +107,40 @@ require_once dirname(__FILE__)."/$level/components/head-meta.php";
         <div class="separator"></div>
 
 <div class="container mb-5">
-
-
 <?php 
-    include "$level/components/cards/browse-hero-card.php";
+    $workers = null;
+    if($output != null){
+        $workers = $output->response;
+    }
+    $rate_types = ["/hr", "/day", "/week", "/project"];
 ?>
 
 <?php 
-    include "$level/components/cards/browse-hero-card.php";
-?>
-<?php 
-    include "$level/components/cards/browse-hero-card.php";
-?>
-<?php 
-    include "$level/components/cards/browse-hero-card.php";
-?>
+    for($wIndx = 0; $wIndx < count($workers); $wIndx++){
+        // echo var_dump($workers[$wIndx]);
+        $fname = $workers[$wIndx]->worker_info->first_name;
+        $lname = $workers[$wIndx]->worker_info->last_name;
+        $name = $fname." ".$lname;
+        $winitials = substr($fname, 0, 1).substr($lname, 0, 1);
 
+        $default_rate = $workers[$wIndx]->worker_info->default_rate;
+        $default_rate_type = $workers[$wIndx]->worker_info->default_rate_type == 0 ? 0 : $rate_types[$workers[$wIndx]->worker_info->default_rate_type - 1];
+        $pricing = $default_rate.$default_rate_type;
+        
+        $rating_average = $workers[$wIndx]->worker_info->rating_average;
+        $total_ratings = $workers[$wIndx]->worker_info->total_ratings;
+
+        $completed_jobs = $workers[$wIndx]->worker_info->completed_jobs;
+
+        $city_info = $workers[$wIndx]->city_info == null ? "Cebu" : $workers[$wIndx]->city_info;
+
+        $worker_id = $workers[$wIndx]->worker_info->user_id;
+
+        $skill_list = $workers[$wIndx]->skillset_info == null ? null : $workers[$wIndx]->skillset_info;
+
+        include "$level/components/cards/browse-hero-card.php";
+    }
+?>
 
 
 </div>
