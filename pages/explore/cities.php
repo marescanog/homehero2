24796a1a7015afa8c82c23e0de7287f75416d21a
marescanog <marescanog@gmail.com>
2021-12-15 09:26:50 +0800

    added cities page and styles

--- pages/explore/cities.php
+++ pages/explore/cities.php
@@ -29,15 +29,94 @@ require_once dirname(__FILE__)."/$level/components/head-meta.php";
     ?>
     <div class="<?php echo $hasHeader ?? ''; ?>">
     <!-- === Your Custom Page Content Goes Here below here === -->
+<div class="jumbotron container">
+<?php
 
-    <!-- =============================================== -->
-    <!-- JUMBOTRON -->
-    <!-- =============================================== -->
-    <div class="jumbotron py-0 mb-auto d-flex flex-lg-end">
-    <h1>Cities</h1>
+//  add a curl request to get list of data to itereate and render for the page
+// NODEPLOYEDPRODLINK
+// Make curl for the personal inforation pagge information vv
+ $url = "http://localhost/slim3homeheroapi/public/get-service-areas"; // DEV
+// $url = ""; // NO DEPLOYED PROD LINK
 
+
+// 1. Initialize
+$ch = curl_init();
+
+// 2. set options
+    // URL to submit to
+    curl_setopt($ch, CURLOPT_URL, $url);
+
+    // Return output instead of outputting it
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+    // Type of request = POST
+    // curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_HTTPGET, 1);
+
+    // Execute the request and fetch the response. Check for errors
+    $output = curl_exec($ch);
+
+    if($output === FALSE){
+        echo "cURL Error:" . curl_error($ch);
+    }
+
+    curl_close($ch);
+
+    // $output =  json_decode(json_encode($output), true);
+    $output =  json_decode($output);
+
+    $cities = [];
+    // Populate data from curl output
+    if($output !== FALSE && $output !== null && $output !== "" && !empty($output)){
+        if($output->success == false){
+        ?>
+            <div class="title-2-container alert alert-danger alert-dismissible fade show" role="alert">
+                <strong>  <?php echo $output->response->status == 500 ? "500 SERVER ERROR": "401 NOT FOUND";?></strong> 
+                <?php echo $output->response->message;?>
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+        <?php
+        } else {
+            // // Get Current Saved Skills
+            // echo var_dump($output);
+            $cities = $output->response->All_Areas;
+            // echo var_dump($cities[0]);
+        }
+    }
+
+    
+?>
+    <div class="pt-5 pb-5">
+        <h1>Cities</h1>
+        <p class="pt-3">Homehero services 12 different cities with 360 different barangays.</p>
+        <img src="../../images/pages/cities/allCities.jpg" class="img-fluid" alt="Responsive image">
+        <h2 class="mt-5">List of Cities & Barangays</h2>
+        <ul class="mt-3">
+            <?php 
+                for($cc = 0; $cc < count($cities); $cc++){
+            ?>
+            <li class="container mt-3">
+                 <h3><?php echo htmlentities($cities[$cc]->city);?></h3>
+                 <ul class="row">
+                    <?php 
+                        for($bb = 0; $bb < count($cities[$cc]->barangays); $bb++){
+                    ?>
+                     <div class="col-12 col-md-6 col-lg-4">
+                        <li><?php echo htmlentities($cities[$cc]->barangays[$bb]);?></li>
+                     </div>
+                     <?php 
+                        }
+                    ?>
+                 </ul>
+            </li>
+            <?php 
+                }
+            ?>
+        </ul>
     </div>
-
+</div>
       
 
 <?php 
