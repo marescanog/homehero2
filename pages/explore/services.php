0718e7a0288e46a1f172e40f694c4b3ed3c9d1ff
marescanog <marescanog@gmail.com>
2021-12-15 09:36:46 +0800

    added services pages info and styles

--- pages/explore/services.php
+++ pages/explore/services.php
@@ -30,13 +30,95 @@ require_once dirname(__FILE__)."/$level/components/head-meta.php";
     <div class="<?php echo $hasHeader ?? ''; ?>">
     <!-- === Your Custom Page Content Goes Here below here === -->
 
-    <!-- =============================================== -->
-    <!-- JUMBOTRON -->
-    <!-- =============================================== -->
-    <div class="jumbotron py-0 mb-auto d-flex flex-lg-end">
-    <h1>Services</h1>
+<div class="jumbotron container">
+<?php
 
+//  add a curl request to get list of data to itereate and render for the page
+// NODEPLOYEDPRODLINK
+// Make curl for the personal inforation pagge information vv
+ $url = "http://localhost/slim3homeheroapi/public/get-project-types"; // DEV
+// $url = ""; // NO DEPLOYED PROD LINK
+
+
+// 1. Initialize
+$ch = curl_init();
+
+// 2. set options
+    // URL to submit to
+    curl_setopt($ch, CURLOPT_URL, $url);
+
+    // Return output instead of outputting it
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+    // Type of request = POST
+    // curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_HTTPGET, 1);
+
+    // Execute the request and fetch the response. Check for errors
+    $output = curl_exec($ch);
+
+    if($output === FALSE){
+        echo "cURL Error:" . curl_error($ch);
+    }
+
+    curl_close($ch);
+
+    // $output =  json_decode(json_encode($output), true);
+    $output =  json_decode($output);
+
+    $project_types = [];
+    // Populate data from curl output
+    if($output !== FALSE && $output !== null && $output !== "" && !empty($output)){
+        if($output->success == false){
+        ?>
+            <div class="title-2-container alert alert-danger alert-dismissible fade show" role="alert">
+                <strong>  <?php echo $output->response->status == 500 ? "500 SERVER ERROR": "401 NOT FOUND";?></strong> 
+                <?php echo $output->response->message;?>
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+        <?php
+        } else {
+            // // Get Current Saved Skills
+            // echo var_dump($output);
+            $project_types = $output->response->All_Services;
+            // echo var_dump($project_types);
+        }
+    }
+
+    
+?>
+<!-- MAIN CONTENT -->
+    <div class="pt-5 pb-5">
+        <h1>Services</h1>
+        <p class="pt-3">Homehero offers 6 different project categories with various subcategories to choose from.</p>
+        <img src="../../images/pages/landing/review_offers.svg" class="img-fluid" alt="Responsive image">
+        <h2 class="mt-5">List of Project types & Subcategories</h2>
+        <ul class="mt-3">
+            <?php 
+                for($cc = 0; $cc < count($project_types); $cc++){
+            ?>
+            <li class="container mt-3">
+                 <h3><?php echo htmlentities($project_types[$cc]->category);?></h3>
+                 <ul class="row">
+                    <?php 
+                        for($bb = 0; $bb < count($project_types[$cc]->subcategory); $bb++){
+                    ?>
+                     <div class="col-12 col-md-6 col-lg-4">
+                        <li><?php echo htmlentities($project_types[$cc]->subcategory[$bb]);?></li>
+                     </div>
+                     <?php 
+                        }
+                    ?>
+                 </ul>
+            </li>
+            <?php 
+                }
+            ?>
+        </ul>
     </div>
+</div>
 
       
 
