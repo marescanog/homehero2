b745141fda7de82fba2ffd67df5ab983a560af8d
marescanog <marescanog@gmail.com>
2021-12-15 09:55:15 +0800

    added careers page

--- pages/explore/careers.php
+++ pages/explore/careers.php
@@ -16,6 +16,12 @@ require_once dirname(__FILE__)."/$level/components/head-meta.php";
 <link rel="stylesheet" href="../../css/landing.css">
 <link rel="stylesheet" href="../../css/footer.css">
 <!-- Add your custom CSS above -->
+<style>
+    .jumbotron {
+    background-image: url("../../images/pages/careers/jumbotron.png");
+    background-size: cover;
+    }
+</style>
 </head>
  <body class="container-fluid m-0 p-0 main-container">  
     <!-- Add your Header NavBar here-->
@@ -33,9 +39,21 @@ require_once dirname(__FILE__)."/$level/components/head-meta.php";
     <!-- =============================================== -->
     <!-- JUMBOTRON -->
     <!-- =============================================== -->
-    <div class="jumbotron py-0 mb-auto d-flex flex-lg-end">
-    <h1>Careers</h1>
-
+    <div class="container">
+    <div class="pt-5 pb-5">
+        <h1>Careers</h1>
+        <h3 class="mt-3 pb-3">Want to be part of the HomeHero Support team?</h3>
+        <div class="jumbotron jumbotron-fluid">
+        <div class="container">
+        </div>
+        </div>
+        <h3>Find the right career option for you.</h3>
+        <div style="max-width:650px;">
+            <p class="pt-3">Homehero has a dedicated support center that is able to answer the tickets of customers. We operate by communicating with our customers through phone and e-mail. We are able to provide a variety of support such as billing issues, disputes and registration requests.</p>
+            <p class="pt-3">To Apply as a HomeHero support agent, send your resume over to homehero@support.com.</p>
+        </div>
+      
+        </div>
     </div>
 
       
